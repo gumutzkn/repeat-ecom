6d287be594ca22c6e7a6e1b627bf5056a30b1226
Görkem <gumutzkn@gmail.com>
2020-09-05 14:50:20 +0300

    added Router

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title"></h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
